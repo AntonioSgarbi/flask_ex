154d124533a17fbf5ec6abf653b703c2f05b1455
antoniosgarbi <antonio.sgarbi@hotmail.com>
2022-08-23 22:51:26 -0300

    add orm

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 # coding: utf-8
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy
+
 
 app = Flask("projeto")
+db = SQLAlchemy(app)
+engine = sqlalchemy.create_engine('sqlite:///firstdb.db')
 
 
 @app.route("/")
@@ -12,6 +17,13 @@ def ola_mundo():
     {"nome": "Playstation", "preco": 1999.99}]
 
     return render_template("ola.html", name=name, products=products), 200
+
+
+class Estudante(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(120), unique=True, nullable=False)
+    registro = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
 
 
 app.run()
